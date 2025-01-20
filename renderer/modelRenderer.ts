9ca4a1da94dd13db9b45fd2afc4a8f0c9c554546
4eb0da <garret.rus@gmail.com>
2025-01-21 02:51:25 +0300

    Fix webgl1 rendering

--- renderer/modelRenderer.ts
+++ renderer/modelRenderer.ts
@@ -134,7 +134,66 @@ const fragmentShader = `
     }
 `;
 
-const vertexShaderHDHardwareSkinning = `#version 300 es
+const vertexShaderHDHardwareSkinningOld = `
+    attribute vec3 aVertexPosition;
+    attribute vec3 aNormal;
+    attribute vec2 aTextureCoord;
+    attribute vec4 aSkin;
+    attribute vec4 aBoneWeight;
+    attribute vec4 aTangent;
+
+    uniform mat4 uMVMatrix;
+    uniform mat4 uPMatrix;
+    uniform mat4 uNodesMatrices[${MAX_NODES}];
+
+    varying vec3 vNormal;
+    varying vec3 vTangent;
+    varying vec3 vBinormal;
+    varying vec2 vTextureCoord;
+    varying mat3 vTBN;
+    varying vec3 vFragPos;
+
+    void main(void) {
+        vec4 position = vec4(aVertexPosition, 1.0);
+        mat4 sum;
+
+        // sum += uNodesMatrices[int(aSkin[0])] * 1.;
+        sum += uNodesMatrices[int(aSkin[0])] * aBoneWeight[0];
+        sum += uNodesMatrices[int(aSkin[1])] * aBoneWeight[1];
+        sum += uNodesMatrices[int(aSkin[2])] * aBoneWeight[2];
+        sum += uNodesMatrices[int(aSkin[3])] * aBoneWeight[3];
+
+        mat3 rotation = mat3(sum);
+
+        position = sum * position;
+        position.w = 1.;
+
+        gl_Position = uPMatrix * uMVMatrix * position;
+        vTextureCoord = aTextureCoord;
+
+        vec3 normal = aNormal;
+        vec3 tangent = aTangent.xyz;
+
+        // https://learnopengl.com/Advanced-Lighting/Normal-Mapping
+        tangent = normalize(tangent - dot(tangent, normal) * normal);
+
+        vec3 binormal = cross(normal, tangent) * aTangent.w;
+
+        normal = normalize(rotation * normal);
+        tangent = normalize(rotation * tangent);
+        binormal = normalize(rotation * binormal);
+
+        vNormal = normal;
+        vTangent = tangent;
+        vBinormal = binormal;
+
+        vTBN = mat3(tangent, binormal, normal);
+
+        vFragPos = position.xyz;
+    }
+`;
+
+const vertexShaderHDHardwareSkinningNew = `#version 300 es
     in vec3 aVertexPosition;
     in vec3 aNormal;
     in vec2 aTextureCoord;
@@ -193,7 +252,171 @@ const vertexShaderHDHardwareSkinning = `#version 300 es
     }
 `;
 
-const fragmentShaderHD = `#version 300 es
+const fragmentShaderHDOld = `
+    precision mediump float;
+
+    varying vec2 vTextureCoord;
+    varying vec3 vNormal;
+    varying vec3 vTangent;
+    varying vec3 vBinormal;
+    varying mat3 vTBN;
+    varying vec3 vFragPos;
+
+    uniform sampler2D uSampler;
+    uniform sampler2D uNormalSampler;
+    uniform sampler2D uOrmSampler;
+    uniform vec3 uReplaceableColor;
+    uniform float uDiscardAlphaLevel;
+    uniform mat3 uTVextexAnim;
+    uniform vec3 uLightPos;
+    uniform vec3 uLightColor;
+    uniform vec3 uCameraPos;
+    uniform bool uHasShadowMap;
+    uniform sampler2D uShadowMapSampler;
+    uniform mat4 uShadowMapLightMatrix;
+    uniform float uShadowBias;
+    uniform float uShadowSmoothingStep;
+
+    const float PI = 3.14159265359;
+    const float gamma = 2.2;
+
+    float distributionGGX(vec3 normal, vec3 halfWay, float roughness) {
+        float a = roughness * roughness;
+        float a2 = a * a;
+        float nDotH = max(dot(normal, halfWay), 0.0);
+        float nDotH2 = nDotH * nDotH;
+
+        float num = a2;
+        float denom = (nDotH2 * (a2 - 1.0) + 1.0);
+        denom = PI * denom * denom;
+
+        return num / denom;
+    }
+
+    float geometrySchlickGGX(float nDotV, float roughness) {
+        float r = roughness + 1.;
+        float k = r * r / 8.;
+        // float k = roughness * roughness / 2.;
+
+        float num = nDotV;
+        float denom = nDotV * (1. - k) + k;
+
+        return num / denom;
+    }
+
+    float geometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float roughness) {
+        float nDotV = max(dot(normal, viewDir), .0);
+        float nDotL = max(dot(normal, lightDir), .0);
+        float ggx2  = geometrySchlickGGX(nDotV, roughness);
+        float ggx1  = geometrySchlickGGX(nDotL, roughness);
+
+        return ggx1 * ggx2;
+    }
+
+    vec3 fresnelSchlick(float lightFactor, vec3 f0) {
+        return f0 + (1. - f0) * pow(clamp(1. - lightFactor, 0., 1.), 5.);
+    }
+
+    void main(void) {
+        vec2 texCoord = (uTVextexAnim * vec3(vTextureCoord.s, vTextureCoord.t, 1.)).st;
+
+        vec4 orm = texture2D(uOrmSampler, texCoord);
+
+        float occlusion = orm.r;
+        float roughness = orm.g;
+        float metallic = orm.b;
+        float teamColorFactor = orm.a;
+
+        vec4 baseColor = texture2D(uSampler, texCoord);
+        vec3 teamColor = baseColor.rgb * uReplaceableColor;
+        baseColor.rgb = mix(baseColor.rgb, teamColor, teamColorFactor);
+        baseColor.rgb = pow(baseColor.rgb, vec3(gamma));
+
+        vec3 normal = texture2D(uNormalSampler, texCoord).rgb;
+        normal = normal * 2.0 - 1.0;
+        normal.x = -normal.x;
+        normal.y = -normal.y;
+        normal = normalize(vTBN * -normal);
+
+        vec3 viewDir = normalize(uCameraPos - vFragPos);
+        vec3 reflected = reflect(-viewDir, normal);
+
+        vec3 lightDir = normalize(uLightPos - vFragPos);
+        float lightFactor = max(dot(normal, lightDir), .0);
+        vec3 radiance = uLightColor;
+
+        vec3 f0 = vec3(.04);
+        f0 = mix(f0, baseColor.rgb, metallic);
+
+        vec3 totalLight = vec3(0.);
+        vec3 halfWay = normalize(viewDir + lightDir);
+        float ndf = distributionGGX(normal, halfWay, roughness);
+        float g = geometrySmith(normal, viewDir, lightDir, roughness);
+        vec3 f = fresnelSchlick(max(dot(halfWay, viewDir), 0.), f0);
+
+        vec3 kS = f;
+        // vec3 kD = vec3(1.) - kS;
+        vec3 kD = vec3(1.);
+        // kD *= 1.0 - metallic;
+        vec3 num = ndf * g * f;
+        float denom = 4. * max(dot(normal, viewDir), 0.) * max(dot(normal, lightDir), 0.) + .0001;
+        vec3 specular = num / denom;
+
+        totalLight = (kD * baseColor.rgb / PI + specular) * radiance * lightFactor;
+
+        if (uHasShadowMap) {
+            vec4 fragInLightPos = uShadowMapLightMatrix * vec4(vFragPos, 1.);
+            vec3 shadowMapCoord = fragInLightPos.xyz / fragInLightPos.w;
+
+            int passes = 5;
+            float step = 1. / float(passes);
+
+            float lightDepth = texture2D(uShadowMapSampler, shadowMapCoord.xy).r;
+            float lightDepth0 = texture2D(uShadowMapSampler, vec2(shadowMapCoord.x + uShadowSmoothingStep, shadowMapCoord.y)).r;
+            float lightDepth1 = texture2D(uShadowMapSampler, vec2(shadowMapCoord.x, shadowMapCoord.y + uShadowSmoothingStep)).r;
+            float lightDepth2 = texture2D(uShadowMapSampler, vec2(shadowMapCoord.x, shadowMapCoord.y - uShadowSmoothingStep)).r;
+            float lightDepth3 = texture2D(uShadowMapSampler, vec2(shadowMapCoord.x - uShadowSmoothingStep, shadowMapCoord.y)).r;
+            float currentDepth = shadowMapCoord.z;
+
+            float visibility = 0.;
+            if (lightDepth > currentDepth - uShadowBias) {
+                visibility += step;
+            }
+            if (lightDepth0 > currentDepth - uShadowBias) {
+                visibility += step;
+            }
+            if (lightDepth1 > currentDepth - uShadowBias) {
+                visibility += step;
+            }
+            if (lightDepth2 > currentDepth - uShadowBias) {
+                visibility += step;
+            }
+            if (lightDepth3 > currentDepth - uShadowBias) {
+                visibility += step;
+            }
+
+            totalLight *= visibility;
+        }
+
+        vec3 color;
+
+        vec3 ambient = vec3(.03);
+        ambient *= baseColor.rgb * occlusion;
+        color = ambient + totalLight;
+
+        color = color / (vec3(1.) + color);
+        color = pow(color, vec3(1. / gamma));
+
+        gl_FragColor = vec4(color, 1.);
+
+        // hand-made alpha-test
+        if (gl_FragColor[3] < uDiscardAlphaLevel) {
+            discard;
+        }
+    }
+`;
+
+const fragmentShaderHDNew = `#version 300 es
     precision mediump float;
 
     in vec2 vTextureCoord;
@@ -1782,7 +2005,7 @@ export class ModelRenderer {
 
         let vertexShaderSource;
         if (this.isHD) {
-            vertexShaderSource = vertexShaderHDHardwareSkinning;
+            vertexShaderSource = isWebGL2(this.gl) ? vertexShaderHDHardwareSkinningNew : vertexShaderHDHardwareSkinningOld;
         } else if (this.softwareSkinning) {
             vertexShaderSource = vertexShaderSoftwareSkinning;
         } else {
@@ -1791,7 +2014,7 @@ export class ModelRenderer {
 
         let fragmentShaderSource;
         if (this.isHD) {
-            fragmentShaderSource = fragmentShaderHD;
+            fragmentShaderSource = isWebGL2(this.gl) ? fragmentShaderHDNew : fragmentShaderHDOld;
         } else {
             fragmentShaderSource = fragmentShader;
         }
