de200bb3e879dcf5e1245ad6b4d3cbe1d65a0aa9
Aleksandr Nefedov <garret.rus@gmail.com>
2026-03-13 17:26:17 +0700

    Upd deps

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist/',
+      'docs/dist/',
+      'node_modules/',
+      '*.js',
+      'third_party/decoder.js',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+);
